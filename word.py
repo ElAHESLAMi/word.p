72daf1dbc18d0589360d57ba70458462fcec2551
Elahe <118528877+ElAHESLAMi@users.noreply.github.com>
2022-12-04 20:01:04 +0330

    Add files via upload

---
+++ word.py
@@ -0,0 +1,15 @@
+letters="Hello my dear. How are you?"
+num_l=letters
+print(len(num_l))
+
+Words="Hello my dear. How are you?"
+num_w=Words
+print(len(num_w.split(" ")))
+
+sentence="hello my dear .how are you"
+num_s=sentence
+print(len(sentence.split(".")))
+
+sentence ="hello my dear .how are you"
+new_sentnce= sentence.replace("r", "P")
+print(new_sentnce)
